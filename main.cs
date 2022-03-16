d9df20a45e9fea9f1b0cc0eb8fa40ff2ba2f71cd
Emmanuel-Fofeyi <99355350+Emmanuel-Fofeyin@users.noreply.github.com>
2022-03-16 15:23:35 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,16 @@
+// Created by: Emmanuel.Israel
+//Created on: March 2022
+//
+// This program displays "Hello, Wold!"
+
+using System;
+
+class Program 
+{
+    public static void Main (string[] args) 
+    {
+        Console.WriteLine ("Hello, World!");
+        Console.WriteLine (" \nDone");
+
+    }
+}
