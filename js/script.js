8c32d1b0f3eb6ff2ee77c0b63e59ea904f31c709
Emmanuel Fofeyin <99355350+Emmanuel-Fofeyin@users.noreply.github.com>
2022-03-16 16:44:53 +0000

    added code to script.js file

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// created my Emmanuel-Fofeyin
+// Created on March 10 2022
+// This file contains information
+
+ {
+  // this function displays "Hello, World!"
+  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+}
